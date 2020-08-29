1ddefa13a5a1f87bc58b748e0c7ec00eef51968a
berlybeer <b.pumacajia.cz@gmail.com>
2020-08-28 19:41:59 -0500

    Bootstrap navigation component

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import jQuery from "jquery";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import jQuery from 'jquery'
 
-window.$ = window.jQuery = jQuery;
+window.$ = window.jQuery = jQuery
 
-import "popper.js";
-import "bootstrap";
-import "./assets/app.scss";
+import 'popper.js'
+import 'bootstrap'
+import './assets/app.scss'
 
-Vue.config.productionTip = false;
+Vue.component('Navbar', require('./components/Navbar.vue').default)
+
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	render: (h) => h(App),
+}).$mount('#app')
