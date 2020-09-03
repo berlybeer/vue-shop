6513ee06106c71fe4a5f908ce62795ff41049646
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-03 12:55:52 -0500

    Firebase integration added

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: 'AIzaSyA-mnHxI21WGTSLEcIartJtsRRcZMUlrLE',
+	authDomain: 'vue-shop-c5eff.firebaseapp.com',
+	databaseURL: 'https://vue-shop-c5eff.firebaseio.com',
+	projectId: 'vue-shop-c5eff',
+	storageBucket: 'vue-shop-c5eff.appspot.com',
+	messagingSenderId: '1000400834220',
+	appId: '1:1000400834220:web:400c565ca78d3a61c0d909',
+	measurementId: 'G-CF8RBRD8X9',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
