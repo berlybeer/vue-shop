1cc1aec5431b247713031b69725481f200e3561c
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-09 22:01:15 -0500

    Limited admin to only auth users

--- src/router.js
+++ src/router.js
@@ -5,10 +5,11 @@ import Admin from './views/Admin.vue'
 import Overview from './views/Overview.vue'
 import Products from './views/Products.vue'
 import Orders from './views/Orders.vue'
+import { fb } from './firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
 	mode: 'history',
 	base: process.env.BASE_URL,
 	routes: [
@@ -21,6 +22,7 @@ export default new Router({
 			path: '/admin',
 			name: 'admin',
 			component: Admin,
+			meta: { requiresAuth: true },
 			children: [
 				{
 					path: 'overview',
@@ -42,11 +44,23 @@ export default new Router({
 		{
 			path: 'about',
 			name: 'about',
-			// route level code-splitting
-			// this generates a separate chunk (about.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
 			component: () =>
 				import(/* webpackChunkName: "about" */ './views/About.vue'),
 		},
 	],
 })
+
+router.beforeEach((to, from, next) => {
+	const requiresAuth = to.matched.some((x) => x.meta.requiresAuth)
+	const currentUser = fb.auth().currentUser
+
+	if (requiresAuth && !currentUser) {
+		next('/')
+	} else if (requiresAuth && currentUser) {
+		next()
+	} else {
+		next()
+	}
+})
+
+export default router
